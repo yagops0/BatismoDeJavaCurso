5f6a6a71059ee1b3969e355c28bb5d60585a070f
yagops0 <yagosalvador34@gmail.com>
2025-01-28 01:51:15 -0300

    Continuando o curso

--- src/condicoes/IfElse.java
+++ src/condicoes/IfElse.java
@@ -32,6 +32,6 @@ public class IfElse {
             System.out.println("Rank: Gennim");
         }
 
-        
+
     }
 }
---
+++ src/scanners/ScannerDoUsuario.java
@@ -0,0 +1,39 @@
+package scanners;
+
+import java.util.Scanner;
+
+public class ScannerDoUsuario {
+
+    public static void main(String[] args) {
+        /*
+         * Scanner = é um jeito de trazer o usuário para nossa aplicação
+         *
+         * Obejtivo: O usuario vai criar um ninja e vamos validar os dados
+         * */
+
+        // abrir o Scanner
+        Scanner caixaTexto = new Scanner(System.in);
+
+        // Receber o nome do ninja
+        System.out.println("Escreva aqui o nome do ninja: ");
+        String nomeDoNinja = caixaTexto.nextLine();
+
+        System.out.println("O nome do ninja é: " + nomeDoNinja);
+
+        // Receber a idade do ninja
+        System.out.println("Escreva aqui a idade o sue ninja: ");
+        int idadeDoNinja = caixaTexto.nextInt();
+
+        System.out.println("A idade do ninja é: " + idadeDoNinja + " anos.");
+
+        // tratamento de dados
+        if (idadeDoNinja >= 18){
+            System.out.println("Esse ninja já é maior de idade e pode ir para missões de fora da aldeia");
+        }else {
+            System.out.println("Esse ninja é muito novo ainda,  precisa treinar mais antes de sair da vila.");
+        }
+
+        // fechar sempre o scanner
+        caixaTexto.close();
+    }
+}
